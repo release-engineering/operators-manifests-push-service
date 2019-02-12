c58d90ddacb3381384efcff47cba78ac63f705ad
Martin Bašti <mbasti@redhat.com>
2019-02-12 13:16:57 +0100

    Log time in UTC
    We want to have consistent time entries in UTC due distributed locations
    
    Signed-off-by: Martin Bašti <mbasti@redhat.com>

--- omps/logger.py
+++ omps/logger.py
@@ -6,8 +6,14 @@
 """Configures logging"""
 
 import logging
+import time
 
 
 def init_logging(conf):
     """Initialize logging module"""
-    logging.basicConfig(level=conf.log_level, format=conf.log_format)
+    logging.Formatter.converter = time.gmtime  # we want to have logs in UTC
+    logging.basicConfig(
+        level=conf.log_level,
+        format=conf.log_format,
+        datefmt="%Y-%m-%dT%H:%M:%SZ",
+    )
