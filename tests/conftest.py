ec645ba1fb9c9f8be71ccfcbc43062831b1122e4
Martin Bašti <mbasti@redhat.com>
2019-02-13 17:07:22 +0100

    Add support for uploading and extracting zip files
    This is first requirement to be able handle operators manifests uploaded
    in zip files.
    
    * OSBS-6688
    
    Signed-off-by: Martin Bašti <mbasti@redhat.com>

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,6 +3,10 @@
 # see the LICENSE file for license
 #
 
+import os
+import shutil
+import zipfile
+
 import pytest
 
 from omps.app import app
@@ -13,3 +17,32 @@ def client():
     client = app.test_client()
 
     yield client
+
+
+@pytest.fixture
+def datadir(tmpdir):
+    """
+    Fixture copies content of data directory to tmp dir to allow immutable work
+    with test data
+
+    :return: tmpdir data path
+    """
+    path =  os.path.join(tmpdir, "data")
+    data_dir = os.path.join(os.path.dirname(__file__), "data")
+    shutil.copytree(data_dir, path)
+    return path
+
+
+@pytest.fixture
+def valid_manifests_archive(datadir, tmpdir):
+    """Construct valid operator manifest data zip archive"""
+
+    path = os.path.join(tmpdir, 'test_archive.zip')
+
+    with zipfile.ZipFile(path, 'w') as zip_archive:
+        # for now we are happy just with empty file in that archive
+        zip_archive.write(
+            os.path.join(datadir, 'empty.yml'),
+            arcname='empty.yml')
+
+    return path
--- tests/test_settings.py
+++ tests/test_settings.py
@@ -44,3 +44,24 @@ def test_log_format():
 
     conf = Config(ConfClass)
     assert conf.log_format == expected
+
+
+def test_zipfile_max_uncompressed_size():
+    """Test of setting zipfile_max_uncompressed_size"""
+    expected = 10
+
+    class ConfClass(DefaultConfig):
+        ZIPFILE_MAX_UNCOMPRESSED_SIZE = expected
+
+    conf = Config(ConfClass)
+    assert conf.zipfile_max_uncompressed_size == expected
+
+
+def test_zipfile_max_uncompressed_size_invalid():
+    """Test of setting invalid zipfile_max_uncompressed_size"""
+
+    class ConfClass(DefaultConfig):
+        ZIPFILE_MAX_UNCOMPRESSED_SIZE = -10
+
+    with pytest.raises(ValueError):
+        Config(ConfClass)
