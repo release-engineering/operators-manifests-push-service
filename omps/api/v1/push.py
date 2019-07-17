f874e6c17f66d5caceac3de3af880249005005a8
Ralph Bean <rbean@redhat.com>
2019-07-17 12:14:54 -0400

    Handle both yaml file extensions.

--- omps/api/v1/push.py
+++ omps/api/v1/push.py
@@ -169,7 +169,7 @@ def get_package_version(quay_org, repo, version=None):
 def _get_reponame_from_manifests(source_dir):
     for filename in os.listdir(source_dir):
         filename = os.path.join(source_dir, filename)
-        if filename.endswith('.yaml'):
+        if filename.endswith('.yaml') or filename.endswith('.yml'):
             with open(filename, 'r') as f:
                 contents = safe_load(f.read())
                 if 'packageName' in contents:
